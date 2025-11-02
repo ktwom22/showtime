abf579b7e61f28265c23c2fd9e03ffbfafd4b3b6
Kyle Twombley <ktwom22@gmail.com>
2025-11-02 07:10:43 -0500

    fix

--- main.py
+++ main.py
@@ -302,7 +302,7 @@ def reset():
     return redirect(url_for("index"))
 
 #if __name__ == "__main__":
-   # app.run(host="0.0.0.0", port=5005, debug=True)
+    #app.run(host="0.0.0.0", port=5005, debug=True)
 
 
 #for flask deployment
